ffb94e5ac1e661b3fe2dc6c4ddfe8d0d177d47af
bigrael123 <bigrael-x@hotmail.com>
2024-06-22 22:00:01 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+year = 2024
+if(2024%4 == 0):
+    print("It's a leap year.")
+else:
+    print("It is not a leap year.")
+up = True
+down = True
+if(up and down == True):
+    print("Make up your mind.")
+else:
+    print("You chose a path.")
